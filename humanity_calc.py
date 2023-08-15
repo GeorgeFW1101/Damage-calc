35a0b0062d38ac3bae13ebf698f9c820adc26f12
GeorgeFW1101 <138554410+GeorgeFW1101@users.noreply.github.com>
2023-08-16 08:44:04 +1000

    Update humanity_calc.py

--- humanity_calc.py
+++ humanity_calc.py
@@ -5,6 +5,6 @@ while(True):
     initial_humanity = int(input("initial humanity(e.g 100): "))
     degree_of_damage = int(input("degree of damage(e.g 20): "))
     degree_of_permanence = float(input("degree of permanence(e.g 0.3): "))
-    result = calculate_humanity(initial_humanity, degree_of_damage,     degree_of_permanence)
+    result = calculate_humanity(initial_humanity, degree_of_damage, degree_of_permanence)
     print("Calculated Humanity:", result)
     print()
