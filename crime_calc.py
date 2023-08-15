6bf72fd5cd7658379d378ccfc695f1aed7bed2ab
GeorgeFW1101 <138554410+GeorgeFW1101@users.noreply.github.com>
2023-08-16 08:34:13 +1000

    Add files via upload

---
+++ crime_calc.py
@@ -0,0 +1,12 @@
+def calculate_humanity(initial_humanity, degree_of_damage, degree_of_permanence):
+    humanity = (initial_humanity - degree_of_damage) * (1 - degree_of_permanence)
+    return humanity
+
+while(True):
+    initial_humanity = int(input("initial humanity(e.g 100): "))
+    degree_of_damage = int(input("degree of damage(e.g 20): "))
+    degree_of_permanence = float(input("degree of permanence(e.g 0.3): "))
+
+    result = calculate_humanity(initial_humanity, degree_of_damage,     degree_of_permanence)
+    print("Calculated Humanity:", result)
+    print()
